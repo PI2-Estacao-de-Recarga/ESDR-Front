6c998c497d79090062a9d1d9de12c84fb7eb92d6
AlineLermen <aline.helena.lermen@gmail.com>
2023-01-10 16:59:06 -0300

    Add payment screen

---
+++ src/screens/choosePaymentScreen/styles.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1, 
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFF',
+    },
+    fontTitle:{
+        fontWeight: 'light',
+        fontSize: 15,
+        fontFamily: theme.fonts.InterLight,
+        marginBottom: 100
+    },
+    button:{
+        backgroundColor: 'black',
+        borderRadius: 10,
+        width: 200,
+        height: 40,
+        marginVertical: 20,
+    },
+    textButton:{
+        fontFamily: theme.fonts.InterRegular,
+        alignSelf: 'center',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 5,
+        paddingBottom: 5,
+        color: '#FFF'
+    }
+});
+
+export default styles;
