10a5ab5df9b8263a77df1216c623c217f291a46a
matheuscvp <matheuscvp12@gmail.com>
2023-01-19 20:06:30 -0300

    adding the first configuration of the redux
    Co-authored-by: Paulo Vítor Silva Abi-Acl <PauloAbiAcl@users.noreply.github.com>

--- src/screens/profileScreen/index.jsx
+++ src/screens/profileScreen/index.jsx
@@ -2,9 +2,19 @@ import React, { useState} from 'react';
 import { View, Text, Image } from 'react-native';
 import { styles } from './styles';
 import BottomTabs, { bottomTabIcons } from '../../components/footerComponent';
+import { useSelector, useDispatch } from 'react-redux'
+import { getUserProfile } from '../../domain/store/user/userSlice';
+
 
 const Profile = () => {
-  const [profile, setProfile] = useState({
+  const dispatch = useDispatch()
+  const userRedux = useSelector((state) => state.user)
+
+  if (!userRedux) {
+    dispatch(getUserProfile())
+  }
+
+  const [profile, setProfile] = useState(userRedux = {
     name: 'Teste',
     email: 'teste@email.com',
     cpf: '***.***.***-**',
