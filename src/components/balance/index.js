26a2cf77b15a95fc08417b8e98180169c2d32e0c
PauloAbiAcl <pv.silva23@hotmail.com>
2023-02-02 04:28:03 -0300

    Não funciona

--- src/components/balance/index.js
+++ src/components/balance/index.js
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
 import styles from './styles';
 
-function Balance() {
-
+function Balance({ tomada }) {
     //Alterar depois para receber de maneira externa ao app se a tomada estará ou não em uso.
     const [estado] = useState({
         status1: "Em uso",
         status2: "Disponível para uso"
     })
-    const [useTime,setUseTime] = useState("00:00")
+    const [useTime, setUseTime] = useState("00:00")
+
+    useEffect(() => {
+        console.log("Tomada", tomada);
+    }, [])
 
     return (
         <View style={styles.container}>
             <View style={styles.item}>
                 <Text style={styles.itemTitle1}>{estado.status1}</Text>
-                <Text style={styles.itemTitle2}>TOMADA 1</Text>
+                <Text style={styles.itemTitle2}>{tomada.name}</Text>
                 <View style={styles.useTime}>
                     <Text style={styles.itemTitle3}>
                         tempo restante
--- src/screens/homeScreen/index.js
+++ src/screens/homeScreen/index.js
@@ -5,7 +5,8 @@ import {
   Modal,
   View,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  ActivityIndicator
 } from 'react-native';
 import styles from './styles';
 import Balance from '../../components/balance';
@@ -14,12 +15,14 @@ import { useNavigation } from '@react-navigation/native';
 import RadioButton from "../../components/radioButton";
 import NavbarComponent from '../../components/navbarComponent';
 import { useEffect } from "react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { getToken } from "../../utils/getToken";
 import jwt_decode from 'jwt-decode';
 import { paymentRepository } from "../../domain/repositories/paymentRepository";
 import { plugRepository } from "../../domain/repositories/plugRepository";
 import { queryClient } from "../../../App"
+
+
 
 const HomePage = () => {
   const navigation = useNavigation();
@@ -29,11 +32,20 @@ const HomePage = () => {
   const [creditAmount, setCreditAmount] = useState(0);
   const [plugName, setPlugName] = useState("")
   const [disabled, setDisabled] = useState(true);
-  const data = [
-    { value: "Celular" },
-    { value: "Patinete/Bike T2" },
-    { value: "Patinete/Bike T3" }
-  ];
+  const [plugInUse, setPlugInUse] = useState([{
+    name: "",
+    dateTimeToDeactivate: ""
+  }])
+  const [plugs, setPlugs] = useState([{
+    name: "",
+    dateTimeToDeactivate: "",
+  }]);
+  const [lista, setLista] = useState([]);
+  const [data, setData] = useState([
+    { value: "Celular", disable: false },
+    { value: "Patinete/Bike T2", disable: false },
+    { value: "Patinete/Bike T3", disable: false }
+  ]);
 
   useEffect(() => {
     const Token = getToken();
@@ -43,11 +55,51 @@ const HomePage = () => {
   }, [])
 
   useEffect(() => {
-    if(creditAmount != 0 && plugName != "")
+    if (creditAmount != 0 && plugName != "")
       setDisabled(false)
     else
       setDisabled(true)
   }, [creditAmount])
+
+  const query = useQuery('getUserPlug', () => plugRepository.getPlug(token, userId), {
+    initialData: plugInUse,
+    enabled: !!token,
+  });
+
+  useEffect(() => {
+    const x = getUserTomada(query.data);
+    console.log("Nomes", x)
+  }, [query.isFetched])
+
+  const queryPlugs = useQuery('getPlugs', () => plugRepository.getPlug(token), {
+    initialData: plugs,
+    enabled: !!token,
+    retry: 3,
+  });
+
+  useEffect(() => {
+    const x = getTomadas(queryPlugs.data);
+    console.log("Plugs", x)
+  }, [query.isFetched])
+
+  function getTomadas(listOfPlugs){
+    var auxList = []
+    listOfPlugs.forEach(item => {
+      auxList.push(item.name)
+    });
+    return auxList;
+  }
+
+  function getUserTomada(listOfPlugs){
+    var auxList = []
+    listOfPlugs.forEach(item => {
+        auxList.push({
+          name: item.name,
+          useFinish: item.dateTimeToDeactivate,
+        })
+    });
+    return auxList;
+  }
 
   const mutationPlug = useMutation(() => plugRepository.setPlug(token, userId, creditAmount, plugName), {
     onSuccess: async (data) => {
@@ -56,7 +108,7 @@ const HomePage = () => {
     },
     onError: (error) => {
       console.log(error.response.data.error)
-    } 
+    }
   });
 
   const mutationOperation = useMutation(() => paymentRepository.createOperation(token, userId, "USO", null, creditAmount), {
@@ -66,8 +118,8 @@ const HomePage = () => {
     },
     onError: (error) => {
       console.log(error.response.data.error)
-    } 
-    
+    }
+
   });
 
   const confirmModal = async () => {
@@ -103,24 +155,29 @@ const HomePage = () => {
   return (
     <View style={styles.container}>
       <NavbarComponent />
-      <Balance />
-      <TouchableOpacity
-        style={styles.button1}
-        onPress={() => navigation.navigate('compra')}
-      >
-        <Text style={styles.textButton}>
-          Comprar Créditos
-        </Text>
-      </TouchableOpacity>
+      {query.isLoading || query.isFetching ?
+        <ActivityIndicator size={80} color="#000000" />
+        :
+        <>
+          <Balance tomada={getUserTomada(query.data)} />
+          <TouchableOpacity
+            style={styles.button1}
+            onPress={() => navigation.navigate('compra')}
+          >
+            <Text style={styles.textButton}>
+              Comprar Créditos
+            </Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity
-        style={styles.button2}
-        onPress={() => setCarregarOption(true)}
-      >
-        <Text style={styles.textButton}>
-          Carregamento
-        </Text>
-      </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button2}
+            onPress={() => setCarregarOption(true)}
+          >
+            <Text style={styles.textButton}>
+              Carregamento
+            </Text>
+          </TouchableOpacity>
+        </>}
       <Modal
         animationType={'slide'}
         transparent={true}
@@ -135,7 +192,7 @@ const HomePage = () => {
               {" "}
               Selecione a tomada que deseja usar:{" "}
             </Text>
-            <RadioButton data={data} onSelect={(value) => selectPlug(value)} />
+            <RadioButton data={data} onSelect={(value) => selectPlug(value)} disable={getTomadas(queryPlugs.data)} />
             <TextInput
               style={styles.input}
               placeholder="Quantidade de créditos:"
@@ -148,7 +205,7 @@ const HomePage = () => {
             <View style={styles.buttons}>
               <TouchableOpacity
                 style={styles.buttonClose}
-                onPress={() => {setCarregarOption(false); setPlugName(""); setCreditAmount(0);}}>
+                onPress={() => { setCarregarOption(false); setPlugName(""); setCreditAmount(0); }}>
                 <Text style={styles.textStyle}>Fechar</Text>
               </TouchableOpacity>
               <TouchableOpacity
@@ -162,6 +219,7 @@ const HomePage = () => {
           </View>
         </View>
       </Modal>
+
       <BottomTabs icons={bottomTabIcons} />
     </View>
   );
